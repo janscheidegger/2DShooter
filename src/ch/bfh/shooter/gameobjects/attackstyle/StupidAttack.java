7f2b3163d6a66afd91b977238feb12959cd31844
Jan Scheidegger <scheideggerj@gmail.com>
2014-11-14 09:44:06 +0100

    Enemy attacks smart

--- src/ch/bfh/shooter/gameobjects/attackstyle/StupidAttack.java
+++ src/ch/bfh/shooter/gameobjects/attackstyle/StupidAttack.java
@@ -4,8 +4,6 @@ import ch.bfh.shooter.Sprites.Map;
 import ch.bfh.shooter.gameobjects.MovableGameObject;
 import ch.bfh.shooter.helper.ShooterConstants;
 
-import java.util.Random;
-
 /**
  * Created by jan on 07/11/14.
  */
@@ -19,6 +17,56 @@ public class StupidAttack extends Attack {
     public StupidAttack() {
     }
 
+
+
+    @Override
+    public void attack(MovableGameObject self, MovableGameObject target, Map map) {
+
+        self.setRight(target.getX() > self.x);
+        self.setLeft(target.getX() < self.x);
+        self.setDown(target.getY() > self.y);
+        self.setUp(target.getY() < self.y);
+
+        if(self.isLeft()) moveLeft(self, map);
+        if(self.isRight()) moveRight(self, map);
+        if(self.isUp()) moveUp(self, map);
+        if(self.isDown()) moveDown(self, map);
+
+
+        if(self.getCollisionRect().intersects(target.getCollisionRect())) {
+            target.hit(attackDmg);
+        }
+
+    }
+
+    private void moveDown(MovableGameObject self, Map map) {
+        int tempY = self.y;
+        self.y ++;
+        checkCollision(self, map);
+        if(bottomRight || bottomLeft) self.y = tempY;
+    }
+
+    private void moveUp(MovableGameObject self, Map map) {
+        int tempY = self.y;
+        self.y --;
+        checkCollision(self, map);
+        if(topLeft || topRight) self.y = tempY;
+    }
+
+    private void moveRight(MovableGameObject self, Map map) {
+        int tempX = self.x;
+        self.x ++;
+        checkCollision(self, map);
+        if(bottomLeft || bottomRight) self.x = tempX;
+    }
+
+    private void moveLeft(MovableGameObject self, Map map) {
+        int tempX = self.x;
+        self.x --;
+        checkCollision(self, map);
+        if(topLeft || bottomLeft) self.x = tempX;
+    }
+
     private void checkCollision(MovableGameObject self, Map map) {
         topLeft = map.getTileType(self.x/ ShooterConstants.TILE_SIZE, self.y/ShooterConstants.TILE_SIZE) == Map.BLOCK;
         topRight = map.getTileType((self.x+ShooterConstants.ENEMY_WIDTH)/ShooterConstants.TILE_SIZE, self.y/ShooterConstants.TILE_SIZE) ==Map.BLOCK;
@@ -26,60 +74,5 @@ public class StupidAttack extends Attack {
         bottomRight = map.getTileType((self.x+ShooterConstants.ENEMY_WIDTH)/ShooterConstants.TILE_SIZE, (self.y+ShooterConstants.ENEMY_HEIGHT)/ShooterConstants.TILE_SIZE) ==Map.BLOCK;
     }
 
-    @Override
-    public void attack(MovableGameObject self, MovableGameObject target, Map map) {
-        int tempX = self.x;
-        int tempY = self.y;
 
-        int heroX = target.getX();
-        int heroY = target.getY();
-        if (heroX > self.x) {
-            self.setRight(true);
-            self.x++;
-        }
-        else {
-            self.setRight(false);
-        }
-        if (heroX < self.x) {
-            self.setLeft(true);
-            self.x--;
-        }
-        else {
-            self.setLeft(false);
-        }
-        if (heroY > self.y) {
-            self.setDown(true);
-            self.y++;
-        }
-        else {
-            self.setDown(false);
-        }
-        if (heroY < self.y) {
-            self.setUp(true);
-            self.y--;
-        }
-        else {
-            self.setUp(false);
-        }
-
-        checkCollision(self, map);
-
-        if ((topLeft || topRight)&& self.isUp()) {
-            self.y = tempY;
-        }
-        if ((bottomLeft || bottomRight) && self.isDown()) {
-            self.y = tempY;
-        }
-        if ((topLeft || bottomLeft) && self.isLeft()) {
-            self.x = tempX;
-        }
-        if ((topRight || bottomRight) && self.isRight()) {
-            self.x = tempX;
-        }
-
-        if(self.getCollisionRect().intersects(target.getCollisionRect())) {
-            target.hit(10);
-        }
-
-    }
 }
